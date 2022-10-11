07d5ea3985487e7dbe5a7304316018df3d4b002d
veowulf112 <devops@Lime-KG-L01.localdomain>
2022-10-11 15:00:25 +0300

    CSS experiments

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
  <h2 style="color: blu" >App Component</h2>
+ <button class="btn btn-success">HELLO</button>
+ <i class="fa fa-home text-success fa-4x"></i>
+ <h1> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi atque consequuntur cupiditate doloremque eaque explicabo fugiat illo illum, iste itaque maiores maxime minus neque nisi odio perferendis quaerat, quod, ut?</h1>
 </template>
 
 <script>
